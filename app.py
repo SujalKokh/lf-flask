bbd70ebbbea660a5bc2f541a836b818815c34c91
Administrator <sujal.kokh@gmail.com>
2021-07-25 16:07:27 +0545

    feat: docker-compose setup for the flask application

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, jsonify
+from flask_mysqldb import MySQL
 import os
+
 # init app
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'database'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'lf-iac'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
 
 @app.route('/', methods=['GET'])
 def get():
-  return jsonify({"msg": "hello world"})
+  cur = mysql.connection.cursor()
+  cur.execute('''SELECT * FROM books''')
+  rv = cur.fetchall()
+  return jsonify({"msg": rv})
 
 # Run server
 if __name__ == '__main__':
